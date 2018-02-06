95f91e301768792b1e6bb820cda643d01a98ebfb
Kristjan Roosild <kristjan.r@gmail.com>
2018-02-06 13:40:41 +0200

    update

--- src/main/java/TestDocker.java
+++ src/main/java/TestDocker.java
@@ -1,13 +1,18 @@
-import org.testcontainers.containers.GenericContainer;
-import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
 
 public class TestDocker {
     public static void main(String[] args) {
         System.out.println();
-//        MySQLContainer mysql = new MySQLContainer();
-//        GenericContainer redis = new GenericContainer("redis:3.0.2").withExposedPorts(6379);
-                PostgreSQLContainer postgres = new PostgreSQLContainer().withDatabaseName("smurfs");
-                System.out.println(postgres.getDriverClassName());
+        // It does not depend on which container you try to create. Tried with these also. Still the same Exception.
+        //        MySQLContainer mysql = new MySQLContainer();
+        //        GenericContainer redis = new GenericContainer("redis:3.0.2").withExposedPorts(6379);
+        System.out.println("Printing out the class name: ");
+        System.out.println(PostgreSQLContainer.class);
+
+        // Exception will be thrown from this line:
+        PostgreSQLContainer postgres = new PostgreSQLContainer().withDatabaseName("smurfs");
+
+        postgres.start();
+        System.out.println(postgres.getDriverClassName());
     }
 }
